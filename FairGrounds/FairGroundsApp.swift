53cea0891f5f0ca216d489b56f8b0141b028a22c
Connor F <cfuchs@ufl.edu>
2024-07-01 19:49:18 -0400

    Initial Commit

---
+++ FairGrounds/FairGroundsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FairGroundsApp.swift
+//  FairGrounds
+//
+//  Created by Connor Fuchs on 7/1/24.
+//
+
+import SwiftUI
+
+@main
+struct FairGroundsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
